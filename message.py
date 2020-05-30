fcadc6d65928fa9913b1e0cc71c50dfc57cfa13f
milindverma13 <63003338+milindverma13@users.noreply.github.com>
2020-05-30 07:31:31 +0530

    Update message.py

--- message.py
+++ message.py
@@ -2,6 +2,6 @@ import smtplib
 s=smtplib.SMTP("smtp.gmail.com",587)
 s.starttls()
 s.login("jyotididwania636@gmail.com","Milindverma@123")
-message=("JENKINS IS NOT WORKING!!!")
+message=("MODEL BUILT CORRECTLY !!!")
 s.sendmail("jyotididwania636@gmail.com","michaelverma0141@gmail.com",message)
 
